f61d56b3ae3f9c83d788a62376769cdb21f1a965
Hjlxc <xcliu0324@gmail.com>
2020-08-17 23:56:36 -0400

    rework on the movieData structure this change will support fetching more than one page

--- src/modules/movieData/index.js
+++ src/modules/movieData/index.js
@@ -4,26 +4,48 @@ import { fetchMovieFromDB } from './utils';
 
 export const movieDataSlice = createSlice({
   name: 'movieData',
-  initialState: { data: [] },
+  initialState: [],
   reducers: {
     setMovieData: (state, action) => {
-      state.data = action.payload;
+      state[action.payload.page - 1] = action.payload;
     },
   },
 });
 
 const { setMovieData } = movieDataSlice.actions;
 
-export const fetchMovieData = () => async (dispatch) => {
+// fetch the data of a given page, if provided
+export const fetchMovieData = (options = {}) => async (dispatch, getState) => {
+  const { movieData } = getState();
+  const page = options.page || 1;
+  if (movieData[page - 1]) return;
   try {
     // fetch and update movie data state
-    const data = await fetchMovieFromDB();
-    dispatch(setMovieData(data.results));
+    const data = await fetchMovieFromDB(options);
+    dispatch(setMovieData(data));
   } catch (e) {
     // handle fetch error, such as retry, etc
   }
 };
 
-export const selectMovieData = (state) => state.movieData.data;
+// retrive the existing movie data from state and return it in order
+export const selectMovieData = ({ movieData }) =>
+  movieData
+    .filter((item) => !!item)
+    .map((item) => item.results)
+    .flat();
+
+// finding the first page that has not being fetched
+// searching from page 1
+export const getFirstUnfetchedPage = ({ movieData }) => {
+  let pageIdx = 0;
+
+  // break the loop when encounter a null or undefined data
+  // then increase the idx by one, this increase will compensate the
+  // offset between page idx and array idx
+  while (movieData[pageIdx++]) {}
+
+  return pageIdx;
+};
 
 export default movieDataSlice.reducer;
--- src/modules/movieData/utils.js
+++ src/modules/movieData/utils.js
@@ -1,7 +1,16 @@
 import { MOVIE_DB_ENDPOINT, MOVIE_DB_API_KEY } from '../../constants';
 
-export const fetchMovieFromDB = async () => {
-  const res = await fetch(`${MOVIE_DB_ENDPOINT}?api_key=${MOVIE_DB_API_KEY}`);
+/**
+ * fetch movie data from data base
+ * @param {*} options
+ * @param options.page specific a certain page to load, default is page 1
+ */
+export const fetchMovieFromDB = async (options = {}) => {
+  const { page } = options;
+  let endpoint = `${MOVIE_DB_ENDPOINT}?api_key=${MOVIE_DB_API_KEY}`;
+  if (page) endpoint += `&page=${page}`;
+
+  const res = await fetch(endpoint);
   if (res.status !== 200)
     throw new Error({ message: `Fetch Data fail with status ${res.status}` });
   return res.json();
