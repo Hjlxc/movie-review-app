ef783ae42704d5819acae7bede14dfaacfa15f32
Hjlxc <xcliu0324@gmail.com>
2020-08-18 22:23:06 -0400

    implement test for movieData reducer

--- src/test/movieData.test.js
+++ src/test/movieData.test.js
@@ -8,13 +8,31 @@ import {
 } from '../modules/movieData';
 import { fetchMovieFromDB } from '../modules/movieData/utils';
 import { MOVIE_DB_ENDPOINT, MOVIE_DB_API_KEY } from '../constants';
-
+const { reducer } = movieDataSlice;
 const {
   setMovieData: setMovieDataAction,
   setLoading: setLoadingAction,
 } = movieDataSlice.actions;
 
 const mockStore = configureMockStore([thunk]);
+
+const testDataPage_1 = {
+  page: 1,
+  totalPage: 2,
+  results: [
+    { title: 'test_title_1', overview: 'test_overview_1' },
+    { title: 'test_title_2', overview: 'test_overview_2' },
+  ],
+};
+
+const testDataPage_2 = {
+  page: 2,
+  totalPage: 2,
+  results: [
+    { title: 'test_title_3', overview: 'test_overview_3' },
+    { title: 'test_title_4', overview: 'test_overview_4' },
+  ],
+};
 
 test('Test setLoadingAction Action', () => {
   const expectTrueAction = {
@@ -36,17 +54,12 @@ test('Test setMovieDataAction Action', () => {
 });
 
 describe('Test fetchMovieData Action', () => {
-  const response = [
-    { title: 'test_title_1', overview: 'test_overview_1' },
-    { title: 'test_title_2', overview: 'test_overview_2' },
-  ];
-
   beforeEach(() => fetch.resetMocks());
 
   it('Test fetchMovieFromDB without option', async () => {
-    fetch.mockResponseOnce(JSON.stringify(response));
+    fetch.mockResponseOnce(JSON.stringify(testDataPage_1));
     const fetchedData = await fetchMovieFromDB();
-    expect(fetchedData).toEqual(response);
+    expect(fetchedData).toEqual(testDataPage_1);
     expect(fetch).toHaveBeenCalledTimes(1);
     expect(fetch).toHaveBeenCalledWith(
       `${MOVIE_DB_ENDPOINT}?api_key=${MOVIE_DB_API_KEY}`
@@ -62,7 +75,7 @@ describe('Test fetchMovieData Action', () => {
   });
 
   it('Test fetchMovieFromDB with valid option', async () => {
-    fetch.mockResponseOnce(JSON.stringify(response));
+    fetch.mockResponseOnce(JSON.stringify(testDataPage_1));
     await fetchMovieFromDB({ page: 2 });
     expect(fetch).toHaveBeenCalledWith(
       `${MOVIE_DB_ENDPOINT}?api_key=${MOVIE_DB_API_KEY}&page=2`
@@ -70,14 +83,53 @@ describe('Test fetchMovieData Action', () => {
   });
 
   it('Test fetchMovieData return the correct action', async () => {
-    fetch.mockResponseOnce(JSON.stringify(response));
+    fetch.mockResponseOnce(JSON.stringify(testDataPage_1));
     const store = mockStore({ movieData: initialState });
     const expectAction = {
       type: 'movieData/setMovieData',
-      payload: response,
+      payload: testDataPage_1,
     };
     await expect(store.dispatch(fetchMovieData())).resolves.toEqual(
       expectAction
     );
+  });
+});
+
+test('Test setLoading Reducer', () => {
+  const store = mockStore({ movieData: initialState }); // initial state has loading as false
+
+  expect(reducer(store.getState().movieData, setLoadingAction(true))).toEqual({
+    ...initialState,
+    loading: true,
+  });
+});
+
+describe('Test setMovieData Reducer', () => {
+  let store;
+  beforeEach(() => {
+    fetch.resetMocks();
+    // override loading to true from initial state
+    // test that after called setMovieData reducer, it will also set loading to false
+    store = mockStore({ movieData: initialState, loading: true });
+  });
+
+  it('Test new state with setMovieData reducer', async () => {
+    fetch.mockResponseOnce(JSON.stringify(testDataPage_1));
+    expect(
+      reducer(
+        store.getState().movieData,
+        await store.dispatch(fetchMovieData())
+      )
+    ).toEqual({ data: [testDataPage_1], loading: false });
+  });
+
+  it('Test new state with setMovieData reducer with page 2', async () => {
+    fetch.mockResponseOnce(JSON.stringify(testDataPage_2));
+    expect(
+      reducer(
+        store.getState().movieData,
+        await store.dispatch(fetchMovieData())
+      )
+    ).toEqual({ data: [undefined, testDataPage_2], loading: false });
   });
 });
