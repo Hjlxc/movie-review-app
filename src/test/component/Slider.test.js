8dd2d0dc7a3f2662bbb74216ac459ff692b25760
Hjlxc <xcliu0324@gmail.com>
2020-08-19 22:22:18 -0400

    Add test for slider

---
+++ src/test/component/Slider.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { Slider } from '../../component';
+
+describe('Test Switch Component', () => {
+  let testRenderer;
+  let instance;
+  const mockOnChange = jest.fn();
+  beforeAll(() => {
+    testRenderer = renderer.create(
+      <Slider
+        min={1}
+        max={5}
+        selectMin={2}
+        selectMax={4}
+        onChange={mockOnChange}
+      />
+    );
+    instance = testRenderer.root;
+  });
+
+  it('Test switch render with correct props', () => {
+    expect(instance.props.min).toBe(1);
+    expect(instance.props.max).toBe(5);
+    expect(instance.props.selectMin).toBe(2);
+    expect(instance.props.selectMax).toBe(4);
+  });
+
+  it('Test onChange Function', () => {
+    instance.props.onChange();
+    expect(mockOnChange).toHaveBeenCalledTimes(1);
+  });
+});
