19f5005fc10c970904ae1e7173f5598a272ffc67
Hjlxc <xcliu0324@gmail.com>
2020-08-19 22:33:16 -0400

    initial app render test

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+﻿import React from 'react';
+import { render, getByTitle } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../app/store';
 import App from '../App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+import { testDataPage_1 } from './modules/movieData.test';
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+describe('Test App', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+  });
+  it('renders learn react link', () => {
+    fetch.mockResponseOnce(JSON.stringify(testDataPage_1));
+    const { getByText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    expect(getByText('Movie Review App')).toBeInTheDocument();
+  });
 });
