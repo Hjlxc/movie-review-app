e4a6c14554255faec0475897ba1c77cd61e42857
Hjlxc <xcliu0324@gmail.com>
2020-08-20 22:32:35 -0400

    export redux for resue by native project

--- index.js
+++ index.js
@@ -1,3 +1,65 @@
 import store from './src/app/store';
+import {
+  initialState as movieFilterIS,
+  sortOptions,
+  setMovieFilterLanguage,
+  setMovieFilterVoting,
+  setMovieFilterAdult,
+  setMovieFilterSearch,
+  setMovieSort,
+  selectLanguageOption,
+  selectFilteredMovie,
+  selectMovieLanguage,
+  selectMovieVoting,
+  selectMovieAdult,
+  selectMovieSearch,
+  selectMovieSort,
+} from './src/modules';
 
-export { store };
+import {
+  initialState as movieDataIS,
+  fetchMovieData,
+  selectMovieData,
+  selectFirstUnfetchedPage,
+  selectHasMoreData,
+  selectLoadingMovieData,
+} from './src/movieData';
+
+const modules = {
+  movieFilter: {
+    initialState: movieFilterIS,
+    actions: {
+      setMovieFilterLanguage,
+      setMovieFilterVoting,
+      setMovieFilterAdult,
+      setMovieFilterSearch,
+      setMovieSort,
+    },
+    selectors: {
+      selectLanguageOption,
+      selectFilteredMovie,
+      selectMovieLanguage,
+      selectMovieVoting,
+      selectMovieAdult,
+      selectMovieSearch,
+      selectMovieSort,
+    },
+    metadata: {
+      sortOptions: Object.keys(sortOptions),
+    },
+  },
+  movieData: {
+    initialState: movieDataIS,
+    actions: { fetchMovieData },
+    selectors: {
+      selectMovieData,
+      selectFirstUnfetchedPage,
+      selectHasMoreData,
+      selectLoadingMovieData,
+    },
+    metadata: {},
+  },
+};
+
+export default store;
+export { modules };
